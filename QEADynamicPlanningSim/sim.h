36b37eb8a522f5f79fbd5fe73abda25906822b98
BusyBox <2894415997@qq.com>
2023-06-20 22:30:00 +0800

    新建仿真程序

---
+++ QEADynamicPlanningSim/sim.h
@@ -0,0 +1,20 @@
+#ifndef SIM_H
+#define SIM_H
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cstdint>
+#include <cstddef>
+#include <random>
+#include <vector>
+#include <queue>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <opencv2/opencv.hpp>
+
+#define PI 3.141592653589793
+
+#endif
